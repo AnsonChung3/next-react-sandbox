ff717157774dd35cf8fed064c56dfe6e91688d71
Anson Chung <ansonchung3@gmail.com>
2025-10-29 21:10:52 +0000

    Add skeleton test for app router usage/implementation

---
+++ next-react-sandbox/src/app/initial-sandbox/page.js
@@ -0,0 +1,10 @@
+export default function InitialSandboxPage() {
+    // This page is intentionally left minimal to test out the app router
+    return (
+        <>
+            <div>
+                <h1>Initial Sandbox Page</h1>
+            </div>
+        </>
+    );
+}
